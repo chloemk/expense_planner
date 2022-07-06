ae346c06b9d9e05025f2b4977731daa5b340ac68
chloemk <minsung.chloe.kim@gmail.com>
2022-07-06 13:06:04 +0900

    refactor: remove useless comments

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,12 +1,9 @@
-// Widget으로 사용하는 것이 아님
-
 class Transaction {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
 
-  // named argument
   Transaction(
       {required this.id,
       required this.title,

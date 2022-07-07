05befed3d4a5c32265e6e4c556c6e8a1cd1a24e3
chloemk <minsung.chloe.kim@gmail.com>
2022-07-07 17:29:12 +0900

    fix: wrap card widget with container

--- lib/main.dart
+++ lib/main.dart
@@ -40,12 +40,13 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Flutter App'),
       ),
       body: Column(
+        // mainAxisAlignment: MainAxisAlignment.start, ! default
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Card(
-            color: Colors.blue,
-            elevation: 5,
-            child:
-                Container(width: double.infinity, child: const Text('Chart!')),
+          Container(
+            width: double.infinity,
+            child: const Card(
+                color: Colors.blue, elevation: 5, child: Text('Chart!')),
           ),
           Column(
             children: transactions

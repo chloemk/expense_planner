6ce237986742ac255f3c2f9b9aa63b331b353db4
chloemk <minsung.chloe.kim@gmail.com>
2022-07-09 21:03:02 +0900

    style: apply colorScheme primary style

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -21,14 +21,16 @@ class TransactionList extends StatelessWidget {
                   margin:
                       const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                   decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)),
+                      border: Border.all(
+                          color: Theme.of(context).colorScheme.primary,
+                          width: 2)),
                   padding: const EdgeInsets.all(10),
                   child: Text(
                     '\$${transactions[idx].amount.toStringAsFixed(2)}',
-                    style: const TextStyle(
+                    style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
-                        color: Colors.purple),
+                        color: Theme.of(context).colorScheme.primary),
                   ),
                 ),
                 Column(

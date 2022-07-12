9c78be1858566db49ec6713dc9d4c33e6aef9dc2
chloemk <minsung.chloe.kim@gmail.com>
2022-07-12 15:07:19 +0900

    feat: create chart bars

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
-  const ChartBar({Key? key}) : super(key: key);
+  final String label;
+  final double spendingAmount;
+  final double spendingPercentageOfTotal;
+
+  const ChartBar(
+      this.label, this.spendingAmount, this.spendingPercentageOfTotal,
+      {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        const SizedBox(
+          height: 4,
+        ),
+        SizedBox(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPercentageOfTotal,
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary)),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(label)
+      ],
+    );
   }
 }

bfbf0d65233a11d14ffc9bb8decb56eaa597be37
chloemk <minsung.chloe.kim@gmail.com>
2022-07-12 12:30:51 +0900

    feat: set chart data

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({Key? key}) : super(key: key);
+  final List<Transaction> recentTransactions;
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0.0;
+
+      for (var tx in recentTransactions) {
+        if (tx.date.day == weekDay.day &&
+            tx.date.month == weekDay.month &&
+            tx.date.year == weekDay.year) {
+          totalSum += tx.amount;
+        }
+      }
+
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
+  }
+
+  const Chart(this.recentTransactions, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      // child: Row(children: <Widget>[]),
+    );
   }
 }

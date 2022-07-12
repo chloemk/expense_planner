85e3cb6c2cee8b48449d79e734c31e4bc7c42c72
chloemk <minsung.chloe.kim@gmail.com>
2022-07-12 16:07:08 +0900

    feat: apply ListTile widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -31,39 +31,35 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (context, idx) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Theme.of(context).colorScheme.primary,
-                                width: 2),
-                            borderRadius: BorderRadius.circular(8)),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          '\$${transactions[idx].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).colorScheme.primary),
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                            '\$${transactions[idx].amount}',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(transactions[idx].title,
-                              style: Theme.of(context).textTheme.headline6),
-                          Text(
-                            DateFormat.yMMMMd("en_US")
-                                .add_jm()
-                                .format(transactions[idx].date),
-                            style: const TextStyle(color: Colors.grey),
-                          )
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      transactions[idx].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMMd("en_US")
+                          .add_jm()
+                          .format(transactions[idx].date),
+                    ),
+                    trailing: const Icon(Icons.delete),
                   ),
                 );
               },
